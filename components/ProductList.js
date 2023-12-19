70a1d86e5f8c7152c66a99cab723ea405c59d8de
fatahidzhar <fatahidzhar.h@gmail.com>
2023-12-19 15:44:21 +0700

    first commit

---
+++ components/ProductList.js
@@ -0,0 +1,18 @@
+import { useDrag } from "react-dnd";
+
+const Product = ({ product }) => {
+  const [, drag] = useDrag({
+    item: { type: "PRODUCT", product },
+  });
+
+  return (
+    <div
+      ref={drag}
+      style={{ border: "1px solid #ccc", padding: "10px", margin: "10px" }}>
+      <h3>{product.name}</h3>
+      <p>{product.price}</p>
+    </div>
+  );
+};
+
+export default Product;
